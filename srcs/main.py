e69b07d90977158fcde788efdc527280c01a9818
agatocherry <agathe.colas@lycam.fr>
2023-12-17 22:06:27 +0100

    Remove TODO comments

--- srcs/main.py
+++ srcs/main.py
@@ -6,9 +6,6 @@ from parsing import *
 from utils import *
 import sys
 
-# TODO print the order of each edge (min to max) (edge order : [(1, 2), (1, 3), (2, 3), (2, 4), (3, 4)])
-# TODO add a more complex graph (with more nodes and edges)
-
 # * Search and give the input file
 if len(sys.argv) != 2:
     print(HRED + "Error: You need to give a file as argument." + RESET)
